4d996a1e812728a4f73bc947f6663bb1c5372789
nigar <nigar@LAPTOP-0DTB49BH>
2019-07-16 10:56:34 -0400

    commit by alam

---
+++ Automation/src/test/java/com/learning/StringClass.java
@@ -0,0 +1,134 @@
+package com.learning;
+
+public class StringClass {
+
+	public static void main(String[] args) {
+
+	   /* 	
+	    	String a="My name is Khan. My name is Bob. My name is Sonoo.";  
+			String replaceString11=a.replaceAll("\\s","");
+			System.out.println(replaceString11);
+	        }} 
+
+
+	  // add to value with connect()method
+		String obj= new String("Smart Tech");
+		System.out.println(obj);
+		System.out.println(obj.concat("  Group"));}}  
+		
+		
+	   // How to reverse string
+       String s= new String("Smart Teach");
+       String newobj = new StringBuffer(s).reverse().toString();
+	   System.out.println(newobj); //} }
+		
+     //System.out.println(newobj.toLowerCase());}}
+      
+       System.out.println(newobj.toUpperCase());}} 
+        
+        
+        String obj= new String("SmartTech");
+		System.out.println(obj);
+		System.out.println(obj.length());//}}
+        System.out.println(obj.charAt(4));//}}
+	    
+		System.out.println(obj.substring(2, 5));
+		System.out.println(obj.substring(2));
+		System.out.println(obj.charAt(4));
+		System.out.println(obj.compareTo("SmartTech"));}}  */
+
+         String a="sarower";  
+         String replaceString11 = a.replaceAll("e","r");
+         System.out.println(replaceString11);}}
+
+	
+/*         //Index location start from 0
+	
+		//System.out.println(name.compareTo(""));
+		
+		//System.out.println(name.concat(" ahmmed"));
+		
+		
+		//System.out.println(name.equals("Ahmmed"));
+	
+	
+		String s1="12345";  
+		System.out.println(s1);
+		int a=Integer.parseInt(s1);
+	    System.out.println(a);
+		
+		String w="aaawwaaaa";  
+		
+		String replaceString1=w.replaceAll("a","e");//replaces all occurrences of "a" to "e"  
+		System.out.println(replaceString1);
+		
+		
+		
+		
+	//	String s2="javatpoint";  
+		  
+	//	System.out.println(s1.isEmpty());  
+	//	System.out.println(s2.isEmpty());
+		
+		
+		if(s1.isEmpty()) {
+			System.out.println("HELLO");
+		}
+		else {
+			System.out.println("HAVE A NICE DAY");
+		}
+		
+		
+		
+		String name="sonoo";  
+		String sf1=String.format("name is %s",name);  
+		String sf2=String.format("value is %f",32.33434);  
+		String sf3=String.format("value is %32.12f",32.33434);//returns 12 char fractional part filling wit
+	
+		  
+		System.out.println(sf1);  
+		System.out.println(sf2);  
+		System.out.println(sf3); 
+	
+		  
+		
+		
+		String js=String.join("-","welcome","to","javatpoint");  
+		System.out.println(js);
+		
+		String s11="this is index of example";//there are 2 's' characters in this sentence  
+		int index1=s11.lastIndexOf('s');//returns last index of 's' char value  
+		System.out.println(index1);//6
+		
+		String s21="sarower";  
+		 
+		System.out.println("string length is: "+s21.length());//10 is the length of javatpoint string  
+		
+		
+		String w="aaawwaaaa";  
+		String replaceString=w.replace('a','e');//replaces all occurrences of 'a' to 'e'  
+		System.out.println(replaceString);  
+		String replaceString1=w.replaceAll("a","e");//replaces all occurrences of "a" to "e"  
+		System.out.println(replaceString1);
+		
+		String a="My name is Khan. My name is Bob. My name is Sonoo.";  
+		String replaceString11=a.replaceAll("\\s","");
+		System.out.println(replaceString11);
+		
+		
+		// trim leading and tailing space	
+		String d1="  hello string   ";  
+		System.out.println(d1+"javatpoint");//without trim()  
+		System.out.println(d1.trim()+"javatpoint");//with trim()  
+		
+		System.out.println(Pattern.matches(".s", "as"));//true (2nd char is s)  
+		System.out.println(Pattern.matches(".s", "mk"));//false (2nd char is not s)  
+		
+		String sdf="3432";
+		System.out.println(sdf);
+		int i=Integer.parseInt(sdf);//NumberFormatException  
+		System.out.println(i);========================================================
+	}
+}
+
+*/
